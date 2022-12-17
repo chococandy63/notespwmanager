28c6f0d2b7053bb1186c33b7589e7aafcc73518b
Riya-Bisht <manasi.riya2003@gmail.com>
2022-12-17 12:23:50 +0530

    added login-page

---
+++ lib/controller/googleauth.dart
@@ -0,0 +1,66 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import '../pages/home.dart';
+
+//creating instances:
+GoogleSignIn googleSignIn = GoogleSignIn();
+FirebaseAuth auth = FirebaseAuth.instance;
+CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+Future<bool> signInWithGoogle(BuildContext context) async {
+  try {
+    final GoogleSignInAccount? googleSignInAccount =
+        await googleSignIn.signIn();
+    if (googleSignInAccount != null) {
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+    
+   //this wasnt in the video i created this to avoid the error
+    final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken);
+
+//firebase authentication:
+    final UserCredential authResult =
+        await auth.signInWithCredential(credential);
+
+    //user returning from firebase authentication:this creates user in authentication panel:
+
+    final User? user = authResult.user;
+    //we need to store user's data: saving name of the user from google sign
+    var userData = {
+      'name': googleSignInAccount.displayName,
+      'provider': 'google',
+      'photoUrl': googleSignInAccount.photoUrl,
+      'email': googleSignInAccount.email,
+    };
+    //if the user already exists(or created their acc on firebase)
+    users.doc(user?.uid).get().then((doc) {
+      if (doc.exists) {
+        //old user
+        doc.reference.update(userData);
+      } else {
+        //new user: fixed the user documentid
+        users.doc(user?.uid).set(userData);
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder:(context)=>const HomePage(),
+          ),
+        );
+      }
+    });
+    }
+  // ignore: non_constant_identifier_names
+  } catch (PlatformException) {
+    // ignore: avoid_print
+    print(PlatformException);
+    // ignore: avoid_print
+    print("Sign in not sucessful!");
+  }
+  throw UnsupportedError('Not yet implemented');
+}
---
+++ lib/pages/login.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:notes/controller/googleauth.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(children: [
+        Padding(
+
+          padding: const EdgeInsets.only(
+            top: 100.0,
+            bottom: 100
+          ),
+          child:  Text(
+          'Write Notes \n & \nManage Passwords',
+          style: GoogleFonts.mansalva(
+            fontSize: 32,
+          ),
+        ),
+        ),
+       
+        
+         Text(
+                "Login",
+                style: GoogleFonts.mansalva(
+                  fontSize: 20,
+                ),
+              ),
+              Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextFormField(
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Enter email id',
+            ),
+          ),
+        ),
+           Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextFormField(
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Enter password',
+            ),
+          ),
+        ), 
+        Padding(padding:const EdgeInsets.symmetric(horizontal:20.0,
+vertical: 20),
+        child: ElevatedButton(
+          onPressed: () {
+            //calling a fn
+            signInWithGoogle(context);
+          },
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Continue With Google",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.mansalva(
+                  fontSize: 20,
+                )
+              ),
+              SizedBox(
+                width:10,
+              ),
+              Image.asset('assets/images/google-login-icon.png',height:36.0,),
+
+            ],
+          ),
+          style:ButtonStyle(
+            backgroundColor:MaterialStateProperty.all(Color.fromARGB(255, 181, 84, 116)
+            ),padding:MaterialStateProperty.all(EdgeInsets.symmetric(
+              vertical: 10,
+            )) )
+        ),
+        ),   
+        
+      ]),
+    ));
+  }
+}
