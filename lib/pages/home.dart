e1444da0696a1954273223ec0fe60eab79e5d130
Riya-Bisht <manasi.riya2003@gmail.com>
2022-12-18 17:22:53 +0530

    changes added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,81 +13,79 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-       body:
-       Center(
-         child: Column(
-           children: [
-             Padding(padding:const EdgeInsets.symmetric(horizontal:20.0,
-vertical: 20),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
               child: ElevatedButton(
-                onPressed: () {
-                  //calling a fn
-                  
-                },
-                // ignore: sort_child_properties_last
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "NOTES",
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.mansalva(
-                        fontSize: 20,
-                      )
-                    ),
-                    const SizedBox(
-                      width:10,
-                    ),
-                    Image.asset('assets/images/notes.jpg',height:36.0,),
-
-                  ],
-                ),
-                style:ButtonStyle(
-                  backgroundColor:MaterialStateProperty.all(const Color.fromARGB(255, 181, 84, 116)
-                  ),padding:MaterialStateProperty.all(const EdgeInsets.symmetric(
-                    vertical: 10,
-                  )) )
-              ),
-              ),
-
-
-               Padding(padding:const EdgeInsets.symmetric(horizontal:20.0,
-vertical: 20),
+                  onPressed: () {
+                    //calling a fn
+                  },
+                  // ignore: sort_child_properties_last
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("NOTES",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.mansalva(
+                            fontSize: 20,
+                          )),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Image.asset(
+                        'assets/images/notes.jpg',
+                        height: 36.0,
+                      ),
+                    ],
+                  ),
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          const Color.fromARGB(255, 181, 84, 116)),
+                      padding:
+                          MaterialStateProperty.all(const EdgeInsets.symmetric(
+                        vertical: 10,
+                      )))),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
               child: ElevatedButton(
-                onPressed: () {
-                  //calling a fn
-                  
-                },
-                // ignore: sort_child_properties_last
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "PASSWORDS",
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.mansalva(
-                        fontSize: 20,
-                      )
-                    ),
-                    const SizedBox(
-                      width:10,
-                    ),
-                    Image.asset('assets/images/password.png',height:36.0,),
-
-                  ],
-                ),
-                style:ButtonStyle(
-                  backgroundColor:MaterialStateProperty.all(const Color.fromARGB(255, 181, 84, 116)
-                  ),padding:MaterialStateProperty.all(const EdgeInsets.symmetric(
-                    vertical: 10,
-                  )) )
-              ),
-              ),
-           ],
-         ),
-       ),   
+                  onPressed: () {
+                    //calling a fn
+                  },
+                  // ignore: sort_child_properties_last
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("PASSWORDS",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.mansalva(
+                            fontSize: 20,
+                          )),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Image.asset(
+                        'assets/images/password.png',
+                        height: 36.0,
+                      ),
+                    ],
+                  ),
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          const Color.fromARGB(255, 181, 84, 116)),
+                      padding:
+                          MaterialStateProperty.all(const EdgeInsets.symmetric(
+                        vertical: 10,
+                      )))),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
