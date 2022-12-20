ccb6484a6747ec69c2c1f127a0ae72b871381890
Snehit Sah <snehitsah@protonmail.com>
2022-12-20 17:00:48 +0530

    login works
    Signed-off-by: Snehit Sah <snehitsah@protonmail.com>

--- lib/controller/googleauth.dart
+++ lib/controller/googleauth.dart
@@ -11,66 +11,49 @@ GoogleSignIn googleSignIn = GoogleSignIn();
 FirebaseAuth auth = FirebaseAuth.instance;
 CollectionReference users = FirebaseFirestore.instance.collection('users');
 
-Future<bool> signInWithGoogle(BuildContext context) async {
-  try {
-    final GoogleSignInAccount? googleSignInAccount =
-        await googleSignIn.signIn();
-    if (googleSignInAccount != null) {
-      final GoogleSignInAuthentication googleSignInAuthentication =
-          await googleSignInAccount.authentication;
+Future signInWithGoogle(BuildContext context) async {
+  final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
+  if (googleSignInAccount != null) {
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
 
-      //this wasnt in the video i created this to avoid the error
-      final AuthCredential credential = GoogleAuthProvider.credential(
-          accessToken: googleSignInAuthentication.accessToken,
-          idToken: googleSignInAuthentication.idToken);
+    //this wasnt in the video i created this to avoid the error
+    final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken);
 
-//firebase authentication:
-      final UserCredential authResult =
-          await auth.signInWithCredential(credential);
+    //firebase authentication:
+    final UserCredential authResult =
+        await auth.signInWithCredential(credential);
 
-      //user returning from firebase authentication:this creates user in authentication panel:
+    //user returning from firebase authentication:this creates user in authentication panel:
 
-      final User? user = authResult.user;
-      //we need to store user's data: saving name of the user from google sign
-      var userData = {
-        'name': googleSignInAccount.displayName,
-        'provider': 'google',
-        'photoUrl': googleSignInAccount.photoUrl,
-        'email': googleSignInAccount.email,
-      };
-      //if the user already exists(or created their acc on firebase)
-      users.doc(user?.uid).get().then((doc) {
-        if (doc.exists) {
-          //old user
-          doc.reference.update(userData);
-
-          Navigator.of(context).pushReplacement(
-          MaterialPageRoute(
-            builder:(context)=>HomePage(),
-          ),
-        );
-        } else {
-          //new user: fixed the user documentid
-          users.doc(user?.uid).set(userData);
-
-          Navigator.of(context).pushReplacement(
-          MaterialPageRoute(
-            builder:(context)=>HomePage(),
-          ),
-        );
-        }
-      });
-    }
-    // ignore: non_constant_identifier_names
-  } catch (PlatformException) {
-    // ignore: avoid_print
-    print(PlatformException);
-    // ignore: avoid_print
-    print("Sign in not sucessful!");
+    final User? user = authResult.user;
+    //we need to store user's data: saving name of the user from google sign
+    var userData = {
+      'name': googleSignInAccount.displayName,
+      'provider': 'google',
+      'photoUrl': googleSignInAccount.photoUrl,
+      'email': googleSignInAccount.email,
+    };
+    //if the user already exists(or created their acc on firebase)
+    users.doc(user?.uid).get().then((doc) {
+      if (doc.exists) {
+        //old user
+        doc.reference.update(userData);
+      } else {
+        //new user: fixed the user documentid
+        users.doc(user?.uid).set(userData);
+      }
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => HomePage(),
+        ),
+      );
+    });
+  } else {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text("Login Failed")),
+    );
   }
-  // ignore: use_build_context_synchronously
-  ScaffoldMessenger.of(context).showSnackBar(
-    const SnackBar(content: Text("yo login successful ;)")),
-  );
-  return true;
 }
